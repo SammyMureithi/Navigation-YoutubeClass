7984b8b21c9045bacff01f9e10d204b3f4517d52
Sammy Mureithi <sammymureithi20@gmail.com>
2022-10-15 18:32:58 +0300

    Navigation Youtube Class

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import DetailsScreen from './Screens/DetailsScreen';
+import HomeScreen from './Screens/HomeScreen';
 
+const HomeStack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+const HomeStackScreen = ( { navigation } ) => (
+  <HomeStack.Navigator>
+    <HomeStack.Screen name='Home' component={HomeScreen}/>
+  </HomeStack.Navigator>
+)
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName='Home'>
+        <Drawer.Screen name='Home' component={ HomeStackScreen} />
+        <Drawer.Screen name='About' component={DetailsScreen}/>
+      </Drawer.Navigator>
+   </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Screens/DetailsScreen.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { Text, View } from 'react-native'
+
+function DetailsScreen() {
+  return (
+      <View>
+          <Text>Hellow From Details Pages</Text>
+    </View>
+  )
+}
+
+export default DetailsScreen
